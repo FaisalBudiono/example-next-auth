ce2c6a6dbace76dbe5c4174374edbdaf4c010906
Faisal Budiono <ucul.project@gmail.com>
2023-09-22 21:41:41 +0700

    Fix refresh token race condition issue if using refresh token rotation
    Refresh token would be called multiple times when opening multiple tabs
    or there are multiple API called in single page almost at the same time
    and the `access token` is expired.

--- src/libs/hooks/auth/useRefreshToken.ts
+++ src/libs/hooks/auth/useRefreshToken.ts
@@ -1,21 +1,34 @@
 'use client'
 
-import { APISingleResponse } from '@libs/api'
+import { APISingleResponse, axios } from '@libs/api'
 import { TokenResponse, UserTokenBundle } from '@libs/auth'
 import { apiUrls, urls } from '@libs/routes'
 import { Subset } from '@libs/typescript-support'
-import axios from 'axios'
 import { signOut, useSession } from 'next-auth/react'
 
 type UpdateToken = Subset<UserTokenBundle>
+type Action = 'start' | 'finish'
+const refreshTokenRequests: number[] = []
 
 export const useRefreshToken = () => {
   const { data: session, update } = useSession()
 
-  const updateSessionOnServer = async (updatedSession: UpdateToken) => {
-    await update(updatedSession)
-  }
-  const updateSessionOnClient = async (updatedSession: UpdateToken) => {
+  const broadcastRefreshAction = new BroadcastChannel('jwt-refresh-action')
+  broadcastRefreshAction.addEventListener('message', (e) => {
+    const action = e.data as Action
+
+    if (action === 'start') {
+      refreshTokenRequests.push(1)
+      return
+    }
+
+    refreshTokenRequests.shift()
+  })
+
+  const broadcastToken = new BroadcastChannel('jwt-token')
+  broadcastToken.addEventListener('message', (e) => {
+    const updatedSession = e.data as UpdateToken
+
     if (session?.token !== undefined) {
       session.token = {
         accessToken:
@@ -24,10 +37,36 @@ export const useRefreshToken = () => {
           updatedSession.token?.refreshToken ?? session.token.refreshToken,
       }
     }
+  })
+
+  const tokenManager = {
+    isRefreshing() {
+      return refreshTokenRequests.length !== 0
+    },
+    start() {
+      refreshTokenRequests.push(1)
+      broadcastRefreshAction.postMessage('start' as Action)
+    },
+    finish() {
+      refreshTokenRequests.shift()
+      broadcastRefreshAction.postMessage('finish' as Action)
+    },
+  }
+
+  const updateSessionOnServer = async (updatedSession: UpdateToken) => {
+    await update(updatedSession)
+  }
+  const updateSessionOnClient = async (updatedSession: UpdateToken) => {
+    broadcastToken.postMessage(updatedSession)
   }
 
   const generateRefreshToken = async () => {
     try {
+      if (tokenManager.isRefreshing()) {
+        return
+      }
+      tokenManager.start()
+
       const res = await axios.post<APISingleResponse<TokenResponse>>(
         apiUrls.refreshToken(),
         {
@@ -45,8 +84,10 @@ export const useRefreshToken = () => {
 
       await updateSessionOnServer(updatedSession)
       await updateSessionOnClient(updatedSession)
+
+      tokenManager.finish()
     } catch (error) {
-      signOut({
+      await signOut({
         callbackUrl: urls.SIGN_IN(),
         redirect: true,
       })
